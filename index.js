96b292331e9d7503e9fb011acc044571d4dc3902
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2023-08-25 22:52:51 +0700

    thien dep trai

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.set("port", 3000);
 const server = http.createServer(app);
 
 app.get("/a", (request, response) => {
-  response.json({ message: "Hey! This is your server response . we fucking done this shittttt again!" });
+  response.json({ message: "Hey! This is your server response " +
+        ". we fucking done this shittttt again! hello vy map" });
 });
 
 server.listen(3300, () => {
