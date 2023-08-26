19c776613ca66999f79645146b2a4418bb412b1b
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2023-08-26 10:31:43 +0700

    update docker-push-ghcr-workflow

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("port", 3000);
 const server = http.createServer(app);
 
 app.get("/a", (request, response) => {
-  response.json({ message: "Hé lô Vy mặp nhất thế giới" });
+  response.json({ message: "Hé lô Vy mặp nhất thế giới sadsadasdsads" });
 });
 
 server.listen(3300, () => {
